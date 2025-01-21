c46ebd212221d314022b017669c8ec5edef8099a
jom1games <jonas@saslona.com>
2025-01-21 19:13:46 +0100

    add close default event

--- src/main/java/me/gaminglounge/itembuilder/Events.java
+++ src/main/java/me/gaminglounge/itembuilder/Events.java
@@ -1,5 +1,14 @@
 package me.gaminglounge.itembuilder;
 
 public class Events {
+
+    public Events() {
+
+        ItemBuilderManager.addBothClickEvent("ItemBuilder:close", (e) -> {
+            e.setCancelled(true);
+            e.getWhoClicked().closeInventory();
+        });
+
+    }
     
 }
